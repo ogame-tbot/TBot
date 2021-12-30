b45f243b892985753409ca551396e5ab9967fcec
PepperNg <stef.hermans@ziggo.nl>
2021-12-30 10:53:45 +0100

    Fix null exception, change GetReturningEspionage to more general GetMissionsInProgress, add check if attack is already in progres

--- TBot/Program.cs
+++ TBot/Program.cs
@@ -1560,7 +1560,6 @@ namespace Tbot {
 										continue;
 
 									var galaxyInfo = ogamedService.GetGalaxyInfo(galaxy, system);
-
 									var planets = galaxyInfo.Planets.Where(p => p != null && p.Inactive && !p.Administrator && !p.Banned && !p.Vacation);
 									List<Celestial> scannedTargets = planets.Cast<Celestial>().ToList();
 									if ((bool) settings.AutoFarm.ExcludeMoons == false) {
@@ -1674,11 +1673,11 @@ namespace Tbot {
 											// Check if probes and slots are available: If not, wait for them.
 											if ((int) tempCelestial.Ships.EspionageProbe < neededProbes) {
 												// Wait for probes to come back to current celestial. If no on-route, continue to next iteration.
-												var returning = Helpers.GetReturningEspionages(tempCelestial.Coordinate, fleets);
-												if (returning != null) {
-													var returningProbes = returning.Sum(f => f.Ships.EspionageProbe);
+												var espionageMissions = Helpers.GetMissionsInProgress(tempCelestial.Coordinate, Missions.Spy, fleets);
+												if (espionageMissions.Any()) {
+													var returningProbes = espionageMissions.Sum(f => f.Ships.EspionageProbe);
 													if (tempCelestial.Ships.EspionageProbe + returningProbes >= neededProbes) {
-														var returningFleets = returning.OrderBy(f => f.BackIn).ToArray();
+														var returningFleets = espionageMissions.OrderBy(f => f.BackIn).ToArray();
 														long probesCount = 0;
 														for (int i = 0; i <= returningFleets.Length; i++) {
 															probesCount += returningFleets[i].Ships.EspionageProbe;
@@ -1715,8 +1714,12 @@ namespace Tbot {
 											slots = UpdateSlots();
 											tempCelestial = UpdatePlanet(tempCelestial, UpdateType.Ships);
 											if (slots.Free > slotsToLeaveFree) {
-												if (Helpers.GetReturningEspionages(tempCelestial.Coordinate, fleets).Any(f => f.Destination.IsSame(target.Celestial.Coordinate))) {
+												if (Helpers.GetMissionsInProgress(tempCelestial.Coordinate, Missions.Spy, fleets).Any(f => f.Destination.IsSame(target.Celestial.Coordinate))) {
 													Helpers.WriteLog(LogType.Warning, LogSender.AutoFarm, $"Probes already on route towards {target.ToString()}.");
+													break;
+												}
+												if (Helpers.GetMissionsInProgress(tempCelestial.Coordinate, Missions.Attack, fleets).Any(f => f.Destination.IsSame(target.Celestial.Coordinate) && f.ReturnFlight == false)) {
+													Helpers.WriteLog(LogType.Warning, LogSender.AutoFarm, $"Attack already on route towards {target.ToString()}.");
 													break;
 												}
 												if ((int) tempCelestial.Ships.EspionageProbe >= neededProbes) {
