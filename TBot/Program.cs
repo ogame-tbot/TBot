c17c912e6f1895cf8d61f10ba03999967b3bb851
Gabriele Coquillard <kokiddp@users.noreply.github.com>
2022-08-23 16:24:00 +0200

    fix missing using directive

--- TBot/Program.cs
+++ TBot/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
 using Newtonsoft.Json.Linq;
 using Tbot.Includes;
 using Tbot.Model;
