0465438e5c703f989fcb7eb941b9745e6f065741
YoloP0wn <anthomaestre@hotmail.fr>
2022-09-01 19:01:50 +0200

    Update TelegramMessenger.cs

--- TBot/Includes/TelegramMessenger.cs
+++ TBot/Includes/TelegramMessenger.cs
@@ -538,14 +538,14 @@ namespace Tbot.Includes {
 									return;
 								}
 								await botClient.SendTextMessageAsync(message.Chat,
-									//"/ghostsleep - Wait fleets return, ghost harvest and sleep for 5hours <code>/ghostsleep 5 Harvest</code>\n" +
+									"/ghostsleep - Wait fleets return, ghost harvest and sleep for 5hours <code>/ghostsleep 5 Harvest</code>\n" +
 									//"/ghostsleepexpe - Wait fleets return, ghost harvest, sleep for 5hours, but keep sending expedition: <code>/ghostsleepexpe 5 Harvest</code>\n" +
 									"/ghost - Ghost fleet for the specified amount of hours\n, let bot chose mission type. Format: <code>/ghost 4</code>\n" +
 									"/ghostto - Ghost for the specified amount of hours on the specified mission. Format: <code>/ghostto 4 Harvest</code>\n" +
 									"/switch - Switch current celestial resources and fleets to its planet or moon at the specified speed. Format: <code>/switch 5</code>\n" +
 									"/deploy - Deploy to celestial with full ships and resources. Format: <code>/delpoy 3:41:9 moon/planet 10</code>\n" +
 									"/jumpgate - jumpgate to moon with full ships [full], or keeps needed cargo amount for resources [auto]. Format: <code>/jumpgate 2:41:9 auto/full</code>\n" +
-									//"/cancelghostsleep - Cancel planned /ghostsleep(expe) if not already sent\n" +
+									"/cancelghostsleep - Cancel planned /ghostsleep(expe) if not already sent\n" +
 									"/spycrash - Create a debris field by crashing a probe on target or automatically selected planet. Format: <code>/jumpgate 2:41:9/auto</code>\n" +
 									"/recall - Enable/disable fleet auto recall. Format: <code>/recall true/false</code>\n" +
 									"/collect - Collect planets resources to JSON setting celestial\n" +
