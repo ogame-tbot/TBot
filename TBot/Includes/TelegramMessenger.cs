23fb42ec215a02d97266d867f2bb9a2981f1ca1b
YoloP0wn <anthomaestre@hotmail.fr>
2022-09-04 15:35:34 +0200

    fix typo

--- TBot/Includes/TelegramMessenger.cs
+++ TBot/Includes/TelegramMessenger.cs
@@ -593,10 +593,10 @@ namespace Tbot.Includes {
 									"/ghost - Ghost fleet for the specified amount of hours\n, let bot chose mission type. Format: <code>/ghost 4</code>\n" +
 									"/ghostto - Ghost for the specified amount of hours on the specified mission. Format: <code>/ghostto 4 Harvest</code>\n" +
 									"/switch - Switch current celestial resources and fleets to its planet or moon at the specified speed. Format: <code>/switch 5</code>\n" +
-									"/deploy - Deploy to celestial with full ships and resources. Format: <code>/delpoy 3:41:9 moon/planet 10</code>\n" +
+									"/deploy - Deploy to celestial with full ships and resources. Format: <code>/deploy 3:41:9 moon/planet 10</code>\n" +
 									"/jumpgate - jumpgate to moon with full ships [full], or keeps needed cargo amount for resources [auto]. Format: <code>/jumpgate 2:41:9 auto/full</code>\n" +
 									"/cancelghostsleep - Cancel planned /ghostsleep(expe) if not already sent\n" +
-									"/spycrash - Create a debris field by crashing a probe on target or automatically selected planet. Format: <code>/jumpgate 2:41:9/auto</code>\n" +
+									"/spycrash - Create a debris field by crashing a probe on target or automatically selected planet. Format: <code>/spycrash 2:41:9/auto</code>\n" +
 									"/recall - Enable/disable fleet auto recall. Format: <code>/recall true/false</code>\n" +
 									"/collect - Collect planets resources to JSON setting celestial\n" +
 									"/msg - Send a message to current attacker. Format: <code>/msg hello dude</code>\n" +
